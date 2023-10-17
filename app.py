7f7c39da3924117e4b938930d16afa722decf8a3
ClairePeng <clairepeng94@gmail.com>
2023-10-17 01:02:53 +0100

    routes drafted for posts, comments, homepage and landing page

--- app.py
+++ app.py
@@ -1,69 +1,63 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from lib.database_connection import get_flask_database_connection
+
 
 # Create a new Flask app
 app = Flask(__name__)
 
+
 # == Your Routes Here ==
 
 # == Homepage == chwitter.com/ ==
-'''
-When we go to the homepage
-We should see the following text:
-
-h1: Y 
-h2: Happening now
-h3: Join today.
-
-[Create Account]<a href="/sign_up">
-
-Already have an account?  
-[Sign in] <a href="/sign_in">
-'''
+@app.route('/', methods = ['GET'])
+def landingpage():
+    return render_template('landingpage.html')
 
 
-# == ALL TWEETS == chwitter.com/home ==
-'''
-When we go to all tweets
-We should see:
-- Y Logo in H1
-- link for the /home
-- link for New Post
-- link for Your Account
-- button for Sign out
+### ===== POSTS ===== ####
 
-- A list of all posts in order of posting where earliest is first
-- For each post:
-    - Name (bold) @Username (italic) - Xh ago or Xdays ago or date
-    - Content
-    - Upvotes
-    - button for upvote/downvoting
-'''
+# == ALL POSTS == chwitter.com/home ==
 
-# == TWEET == chwitter.com/posts/<id> ==
-'''
-When we go to a single tweet
-We should see:
-- Y Logo in H1
-- link for the /home
-- link for New Post
-- link for Your Account
-- button for Sign out
+@app.route('/home', methods = ['GET'])
+def home():
+    connection = get_flask_database_connection(app)
+    # post_repository = PostRepository(connection)
+    # post = post_repository.all()
+    return render_template('home.html', errors=None)
 
-<b>{Name}</b> @{Username} - {hours ago or date}h
-{content}
-{upvotes}
+# == CREATE POST PAGE ==
+@app.route('/posts/new', methods = ['GET'])
+def create_post_page():
+    connection = get_flask_database_connection(app)
+    # post_repository = PostRepository(connection)
+    # post = post_repository.all()
+    return render_template('new_post.html', errors=None)
 
-- button for delete
-'''
+# == CREATE POST ==
+@app.route('/posts', methods = ['POST'])
+def create_post():
+    connection = get_flask_database_connection(app)
+    # post_repository = PostRepository(connection)
+    # post = post_repository.all()
+    return render_template('home.html', errors=None)
 
-# == DELETE TWEET == chwitter.com/posts/<id>/delete --> chwitter.com/home ==
-'''
-When we click delete for a single tweet
-We see "tweet successfully deleted"
-Then we see the homepage refreshed without this tweet
-'''
+# == SHOW POST == chwitter.com/posts/<id> ==
+@app.route('/posts/<id>', methods = ['GET'])
+def show_post(id):
+    connection = get_flask_database_connection(app)
+    # post_repository = PostRepository(connection)
+    # post = post_repository.all()
+    return render_template('show_post.html', errors=None)
+
+# == DELETE POST == chwitter.com/posts/<id>/delete --> chwitter.com/home ==
+@app.route('/posts/<id>/delete', methods = ['POST'])
+def delete_post(id):
+    connection = get_flask_database_connection(app)
+    # post_repository = PostRepository(connection)
+    # post = post_repository.all()
+    return redirect(url_for('home'))
+
 
 # == UPVOTE/DOWNVOTE TWEET == page refresh
 '''
@@ -71,8 +65,67 @@ When we click upvote or downvote, we upvote or downvote a tweet
 We will refresh the page and see that the number of upvotes/downvotes has increase by 1.
 ** We can only upvote/downvote once.
 '''
+# TODO These only work on the homepage, not on the individual post page.
+@app.route('/posts/<id>/upvote', methods = ['POST'])
+def upvote_post(id):
+    connection = get_flask_database_connection(app)
+    # post_repository = PostRepository(connection)
+    # post = post_repository.all()
+    return redirect(request.referrer)
+
+@app.route('/posts/<id>/downvote', methods = ['POST'])
+def downvote_post(id):
+    connection = get_flask_database_connection(app)
+    # post_repository = PostRepository(connection)
+    # post = post_repository.all()
+    return redirect(request.referrer)
 
 
+### ===== COMMENTS ===== ####
+
+# == SHOW COMMENT == chwitter.com/posts/<post_id>/comments/<id> ==
+@app.route('/posts/<post_id>/comments/<id>', methods = ['GET'])
+def show_comment(post_id, id):
+    connection = get_flask_database_connection(app)
+    # post_repository = PostRepository(connection)
+    # post = post_repository.all()
+    return render_template('show_comment.html', errors=None)
+
+# == DELETE POST == chwitter.com/<post_id>/comments/<id>/delete --> chwitter.com/home ==
+@app.route('/posts/<post_id>/comments/<id>/delete', methods = ['POST'])
+def delete_comment(post_id, id):
+    connection = get_flask_database_connection(app)
+    # post_repository = PostRepository(connection)
+    # post = post_repository.all()
+    return redirect(url_for('home')) #return home or parent post
+
+
+# == UPVOTE/DOWNVOTE COMMENT == page refresh
+'''
+When we click upvote or downvote, we upvote or downvote a comment
+We will refresh the page and see that the number of upvotes/downvotes has increase by 1.
+** We can only upvote/downvote once.
+'''
+# TODO These only work on the homepage, not on the individual post page.
+@app.route('/posts/<post_id>/comments/<id>/upvote', methods = ['POST'])
+def upvote_comment(post_id, id):
+    connection = get_flask_database_connection(app)
+    # post_repository = PostRepository(connection)
+    # post = post_repository.all()
+    return redirect(request.referrer) #return home or parent post
+
+@app.route('/posts/<post_id>/comments/<id>/downvote', methods = ['POST'])
+def downvote_comment(post_id, id):
+    connection = get_flask_database_connection(app)
+    # post_repository = PostRepository(connection)
+    # post = post_repository.all()
+    return redirect(request.referrer) #return home or parent post
+
+
+
+
+
+### ===== USERS ===== ####
 
 # == USER == chwitter.com/users/<id> ==
 '''
@@ -105,11 +158,19 @@ We should see:
 
 '''
 
-# == CREATE NEW USER ==
+# == CREATE NEW USER == chwitter.com/sign_up==
+@app.route('/sign_up', methods = ['GET'])
+def sign_up():
+    return "<h1>TODO: CREATE SIGN UP PAGE<H1>"
 
-# == SIGN IN ==
 
-# == SIGN OUT ==
+# == SIGN IN == chwitter.com/login ==
+@app.route('/login', methods = ['GET'])
+def login():
+    return "<h1>TODO: CREATE LOGIN PAGE<H1>"
+
+
+# == SIGN OUT == chwitter.com/logout ==
 
 
 
